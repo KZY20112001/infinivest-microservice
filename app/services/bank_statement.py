18b4279efc63090e3c81ac5c89b05c9c46530f5b
KZY20112001 <43703608+KZY20112001@users.noreply.github.com>
2024-12-01 01:03:56 +0800

    set up jupyter notebook

---
+++ app/services/bank_statement.py
@@ -0,0 +1,83 @@
+from werkzeug.datastructures import FileStorage
+import pandas as pd
+
+from app.utils.bank_statement_parsers import parse_OCBC_bank_statement, parse_SC_bank_statement
+from app.utils.openai_client import classify_transactions, classify_portfolio 
+
+def classify_profile(bank_statement: FileStorage, bank_name: str)->str:
+    """
+    Takes in a bank statement (pdf) and the corresponding bank name. 
+    Returns a recommended portfolio style based on the bank statement. 
+    """
+    transactions: list = []
+    match bank_name: 
+        case "OCBC": 
+            transactions = parse_OCBC_bank_statement(bank_statement)
+        case "SC": 
+            transactions = parse_SC_bank_statement(bank_statement)
+            
+    # create a dataframe from the transactions
+    df = create_dataframe(transactions)
+    
+    # add categorization for each expense
+    df = add_category(df)
+    
+    expenses_dict = calc_expenses(df)
+    
+    return classify_portfolio(expenses_dict)
+    
+
+def create_dataframe(transactions: list) -> pd.DataFrame: 
+    """
+    Takes in a list of transactions and return a dataframe after filtering out deposit-related transactions. 
+    """
+    df = pd.DataFrame(transactions) 
+    df = df.drop(df[df.type == 'deposit'].index) # drop deposit  
+    return df.drop(columns=['type']).reset_index(drop=True)  # only return expenses
+
+def add_category(df: pd.DataFrame) -> pd.DataFrame: 
+    """
+    Use OpenAI LLM to classify transactions in the dataframe into 5 categories: essentials, discretionary, debt, savings, and miscellaneous. 
+    Appends a new column called "category" for each transactions. 
+    Returns the updated df 
+    """
+    max_index = len(df)
+    descriptions = df['description']
+    category_dict = {}
+    current = ""
+    count = 0
+    for i in range(len(descriptions)):
+        cur = str(i) + " " + descriptions.iloc[i]
+        current += cur
+        count += 1
+        if count == 50 or i == len(df) - 1:
+            categories = classify_transactions(current).split('\n')
+            for category in categories: 
+                array = category.strip().split(' ')
+                if len(array) != 2 or not array[0].isnumeric() or int(array[0]) < 0 or int(array[0]) >= max_index: 
+                    continue
+                category_dict[array[0]] = array[1] 
+            current = ""
+            count = 0
+        else:
+            current += "\n"
+    df['category'] = df.index.map(lambda x: category_dict.get(str(x), 'miscellaneous'))
+    return df
+
+
+def calc_expenses(df: pd.DataFrame) -> dict: 
+    """
+    Calculate the expenses for each category as a percentage (ignoring "miscellaneous" category)
+    Returns a dict containing percentages
+    """
+    categories = ['essentials', 'discretionary', 'debt', 'savings']
+    expenses_dict = {category: 0 for category in categories}
+    total_amount = df[df['category'] != 'miscellaneous']['amount'].sum()
+    
+    # Update percentages for each category except 'miscellaneous'
+    for _, row in df.iterrows():
+        if row['category'] != 'miscellaneous':
+            expenses_dict[row['category']] += (row['amount'] / total_amount) * 100
+    
+    return expenses_dict
+    
---
+++ app/utils/bank_statement_parsers/ocbc.py
@@ -0,0 +1,100 @@
+import re
+
+from werkzeug.datastructures import FileStorage
+import pymupdf, fitz
+
+def is_indented(line:str) -> bool:
+    return len(line) - len(line.lstrip()) >= 8
+
+def parse_OCBC_bank_statement(bank_statement: FileStorage) -> list: 
+    transactions = []
+    with fitz.open(bank_statement, filetype="pdf") as doc:
+        for pageNo in range(len(doc)):
+            current_transaction: dict = {}
+                
+            if pageNo == 1 or pageNo == len(doc)-1:
+                continue
+            
+            page:str = doc[pageNo].get_text()
+            lines = page.split('\n')
+            #skip first lines till transactions lines are reached i.e skip till "Account No."
+            i = 0
+            while i < len(lines):
+                curLine = lines[i]
+                if pageNo == 0 and i == 0 and curLine != "OCBC Bank": 
+                    print("Current PDF is not OCBC Bank Statement")
+                    return
+                words = lines[i].split(' ')
+                i+=1
+                if (len(words) >= 2 and words[0] == "Account" and words[1] == "No."): 
+                    i+=1
+                    break
+
+            if pageNo == 0: 
+                cur_balance = float(lines[i].strip().replace(",", ""))
+                i+=2
+            
+            #get transactions
+            while i < len(lines): 
+                line = lines[i]
+                i+=1
+                
+                if not line.strip():
+                    continue
+                
+                # if no more transactions are left, break the loop in the second final page 
+                if pageNo == len(doc) - 2 and line == 'BALANCE C/F': 
+                    break
+                
+                if re.match(r'\d{2} \w{3}', line.strip()):
+                        # If there's an ongoing transaction and we encounter a new date, complete the transaction
+                        if 'posting_date' in current_transaction and 'transaction_date' in current_transaction:
+                            # Determine if it's a deposit or withdrawal based on balance change
+                            if current_transaction['balance'] > cur_balance:
+                                current_transaction['type'] = 'deposit'
+                            else:
+                                current_transaction['type'] = 'withdrawal'
+
+                            # Store the transaction
+                            transactions.append(current_transaction)
+
+                            # Update the previous balance
+                            cur_balance = current_transaction['balance']
+                            current_transaction = {}
+
+                        # If there is already a transaction date in progress, this line is the "posting date"
+                        if 'transaction_date' in current_transaction:
+                            current_transaction['posting_date'] = line.strip()
+                        else:
+                            # Start a new transaction with the transaction date
+                            current_transaction = {'transaction_date': line.strip()}
+
+                    # Check for transaction amount (indented number)
+                elif line.strip().replace(',', '').replace('.', '').isdigit() and is_indented(line):
+                    amount = float(line.strip().replace(',', ''))
+                    if 'amount' not in current_transaction:
+                        current_transaction['amount'] = amount
+                    else:
+                        current_transaction['balance'] = amount
+
+                # Append description details
+                else:
+
+                    if 'description' in current_transaction:
+                        current_transaction['description'] += ' ' + line.strip()
+                    else:
+                        current_transaction['description'] = line.strip()
+
+            # Handle the last transaction after the loop
+            if current_transaction and 'balance' in current_transaction:
+                if current_transaction['balance'] > cur_balance:
+                    current_transaction['type'] = 'deposit'
+                else:
+                    current_transaction['type'] = 'withdrawal'
+                transactions.append(current_transaction)
+               
+    transactions = [
+        {key: value for key, value in transaction.items() if key not in ['posting_date', 'transaction_date']}
+        for transaction in transactions
+    ]
+    return transactions
